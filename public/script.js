f21e86f29a0b7507bf6bac30eda3172463b0cef2
Kauan <kauan346goncalves@gmail.com>
2023-02-28 22:47:25 -0300

    ajeitando html

--- public/script.js
+++ public/script.js
@@ -1,17 +1,17 @@
 const carteira = document.querySelector('#carteira')
 const sobra = document.querySelector('#sobra')
-const divida = document.querySelector('#divida')
+const despesas = document.querySelector('#divida')
 const ul = document.querySelector('#list')
 
 const gastos = [
-    {categoria: "mercado", percent: "16", percent2: "29"}, 
-    {categoria: "saúde", percent: "11", percent2: "16"},
-    {categoria: "lazer", percent: "8", percent2: "14"},
-    {categoria: "transporte", percent:"9", percent2: "13" },
-    {categoria: "restaurante", percent:"11", percent2: "17"},
-    {categoria: "viajem",percent: "12", percent2: "18"}, 
-    {categoria: "imposto", percent: "3", percent2: "7"},
-    {categoria: "outros", percent: "3", percent2: "5"}
+    {categoria: "Mercado", percent: "16", percent2: "29"}, 
+    {categoria: "Saúde", percent: "11", percent2: "16"},
+    {categoria: "Lazer", percent: "8", percent2: "14"},
+    {categoria: "Transporte", percent:"9", percent2: "13" },
+    {categoria: "Restaurante", percent:"11", percent2: "17"},
+    {categoria: "Viajem",percent: "12", percent2: "18"}, 
+    {categoria: "Imposto", percent: "3", percent2: "7"},
+    {categoria: "Outros", percent: "3", percent2: "5"}
 ]
 const percents = gastos.map(( {percent})=>{
     return percent 
@@ -22,12 +22,10 @@ const percents2 = gastos.map(( {percent2})=>{
 const categorias = gastos.map(({categoria})=>
 {return categoria}, "")
 
-
 function getSald(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
-  
 }
   
 function createExtract(){
@@ -41,20 +39,14 @@ function createExtract(){
     const expensesValue = calcPercentsOfBalance(result, sald)
     const expenseTotal = expensesValue.reduce((acc, current)=> acc + current ,0)
 
-    calcTotalExpenses(expenseTotal, sald)
     
-    carteira.innerHTML = sald
-
-
+    carteira.innerHTML = `R$ ${sald}`
     const valor = sald - expenseTotal
 
-    if(valor >= 0)
+    if(valor)
     {
-        sobra.innerHTML=valor
-       divida.innerHTML = 0
-    } else{
-        divida.innerHTML = valor
-        sobra.innerHTML = 0
+         despesas.innerHTML = `R$ ${expenseTotal}`
+         sobra.innerHTML = `R$ ${valor}`
     }
     printExpenses(expensesValue)
     console.log("saldo: " + sald, "despesas:  "  + expenseTotal)
@@ -62,14 +54,7 @@ function createExtract(){
 }
 
 function calcPercentsOfBalance(arr, sald){
-    return arr.map((curr)=> (sald/100 * curr).toFixed(2))
-}
-
-function calcTotalExpenses(expenseTotal,sald){
-    if(sald >= expenseTotal){
-       return 
-    } else return 
-    
+    return arr.map((curr)=> Math.ceil(sald/100 * curr, 1))
 }
 
 function printExpenses(expensesValue){
