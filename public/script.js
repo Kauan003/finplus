5440c7bb86f032066fdab1ae1d5a3e654274398a
Kauan <kauan346goncalves@gmail.com>
2023-03-05 22:34:47 -0300

    cleaning the code

--- public/script.js
+++ public/script.js
@@ -1,67 +1,50 @@
-const carteira = document.querySelector('#carteira')
-const sobra = document.querySelector('#sobra')
-const despesas = document.querySelector('#divida')
+const balanceText= document.querySelector('#saldo')
+const remainingBalanceText = document.querySelector('#sobra')
+const expensesText = document.querySelector('#divida')
 const ul = document.querySelector('#list')
 
-const gastos = [
-    {categoria: "Mercado", percent: "16", percent2: "29"}, 
-    {categoria: "Saúde", percent: "11", percent2: "16"},
-    {categoria: "Lazer", percent: "8", percent2: "14"},
-    {categoria: "Transporte", percent:"9", percent2: "13" },
-    {categoria: "Restaurante", percent:"11", percent2: "17"},
-    {categoria: "Viajem",percent: "12", percent2: "18"}, 
-    {categoria: "Imposto", percent: "3", percent2: "7"},
-    {categoria: "Outros", percent: "3", percent2: "5"}
+const Spending = [
+    {type: "Mercado", percent: "16", percent2: "29"}, 
+    {type: "Saúde", percent: "11", percent2: "16"},
+    {type: "Lazer", percent: "8", percent2: "14"},
+    {type: "Transporte", percent:"9", percent2: "13" },
+    {type: "Restaurante", percent:"11", percent2: "17"},
+    {type: "Viajem",percent: "12", percent2: "18"}, 
+    {type: "Imposto", percent: "3", percent2: "7"},
+    {type: "Outros", percent: "3", percent2: "5"}
 ]
-const percents = gastos.map(( {percent})=>{
-    return percent 
-}, "")
-const percents2 = gastos.map(( {percent2})=>{
-    return percent2
-}, "")
-const categorias = gastos.map(({categoria})=>
-{return categoria}, "")
+
+const percents = Spending.map(( {percent})=>{ return percent }, "")
+const percents2 = Spending.map(( {percent2})=>{return percent2}, "")
+const categorias = Spending.map(( {type})=>{return type}, "")
 
 function getSald(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-const sald = getSald(2100,3500)
 
+const balance = getSald(2100,3500)
+const expensesValue = allExpenses()
+
+function createExtract(){
+    const expenseTotal = expensesValue.reduce((acc, current)=> acc + current ,0)
+    const remainingBalance = balance - expenseTotal
+    balanceText.innerHTML = `R$ ${balance}`
+    expensesText.innerHTML = `R$ ${expenseTotal}`
+    remainingBalanceText.innerHTML = `R$ ${remainingBalance}`
+    printExpenses(expensesValue)
+}
+
+function calcPercentsOfBalance(arr, balance){
+    return arr.map((curr)=> Math.ceil(balance/100 * curr, 1))
+}
 function allExpenses(){
     const result = []
     for(i=0; i < percents.length; i++){
         result.push(getSald(percents[i], percents2[i]))
     }
-
-    return calcPercentsOfBalance(result, sald)
-}
-
-const expensesValue = allExpenses()
-function createExtract(){
-    
-    
-    const expenseTotal = expensesValue.reduce((acc, current)=> acc + current ,0)
-    
-    carteira.innerHTML = `R$ ${sald}`
-    const valor = sald - expenseTotal
-
-    if(valor)
-    {
-         despesas.innerHTML = `R$ ${expenseTotal}`
-         sobra.innerHTML = `R$ ${valor}`
-    }
-    printExpenses(expensesValue)
-    console.log("saldo: " + sald, "despesas:  "  + expenseTotal)
-    console.log(categorias, expensesValue)
-
-    console.log(result)
-    
-}
-
-function calcPercentsOfBalance(arr, sald){
-    return arr.map((curr)=> Math.ceil(sald/100 * curr, 1))
+    return calcPercentsOfBalance(result, balance)
 }
 
 function printExpenses(expensesValue){
@@ -74,6 +57,7 @@ function printExpenses(expensesValue){
 }
 
 
+//GRAPHS
 var canvas = document.querySelector('#graph');
 var ctx = canvas.getContext('2d')
 
@@ -85,7 +69,6 @@ const data = {
 
     }]
 };
-
 
 function createGraph(){
     let myPieChart = new Chart(ctx,{
