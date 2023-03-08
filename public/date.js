7c6696fbe9bbaa2cd64d8053a3e54a8b3bbdb4da
Kauan <kauan346goncalves@gmail.com>
2023-03-07 23:10:09 -0300

    script to get the current month

--- public/date.js
+++ public/date.js
@@ -4,5 +4,4 @@ const month = date.toLocaleString('default', { month: 'long' });
 const capitalizedMonth = month.charAt(0).toUpperCase() + month.slice(1);
 
 text.innerHTML= `<h1> ${capitalizedMonth} </h1>`
-
 text.style.marginLeft='40px'
